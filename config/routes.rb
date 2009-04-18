f43f3773c7e1c9af774666309735a8fbffa48e90
Hiroaki Wakabayashi <wak@debian.example.jp>
2009-04-19 00:59:59 +0900

    routes.rbをログイン実装に伴い修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root(:controller => 'g_script', :action => 'index')
+  map.root(:controller => 'actors', :action => 'index')
 
   # The priority is based upon order of creation: first created -> highest priority.
 
